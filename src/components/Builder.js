5f5b4292196217b30ba5d449d7dbfbdb7caa6c25
Carl Dungca <carl.dungca@yahoo.com>
2019-07-26 21:39:31 -0700

    Renamed category class to categories for clarity

--- src/components/Builder.js
+++ src/components/Builder.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Foodcard from './Foodcard';
 import Search from './Search';
 
+
 const foods = [
 	{
 		type: 'Protein',
@@ -33,7 +34,7 @@ export default function Builder() {
 		<div>
 				<h1>MealBuilder</h1>
 				<h3>Pick foods from at least 3 categories</h3>
-				<div className="category">
+				<div className="categories">
 					{foodCards}
 					<Search />
 				</div>
