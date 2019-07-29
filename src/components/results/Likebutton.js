3fa5ec010b2bab752923a05f85dbe5dafd05ea16
Carl Dungca <carl.dungca@yahoo.com>
2019-07-29 13:17:33 -0700

    Added Likebutton component that was forgotten in previous commits

---
+++ src/components/results/Likebutton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+// NOTE: need to make the location of this responsive
+// Maintain constant distance above card title
+export default function Likebutton() {
+	return (
+		<button><i class="far fa-heart"></i></button>
+	)
+}
