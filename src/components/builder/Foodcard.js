99991d5a8f8cf23e563b1e3a8cf6ba80a79c0c4d
Carl Dungca <carl.dungca@yahoo.com>
2019-07-29 12:10:54 -0700

    Added styling to labels and checkbox inputs to make them look like the mockup

--- src/components/builder/Foodcard.js
+++ src/components/builder/Foodcard.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import Addfood from './Addfood';
-
 const uuidv4 = require('uuid/v4');
 
-// We need each label to correspond to a unique ID
-// For each content, return a label with ID and checkbox corresponding to that ID
+
 export default function Foodcard({title, contents, icon}) {
+
+	// Assign each foodcard a label and invisible checkbox to send information to API
 	const foodContents = contents.map((item) => {
 		const labelId = uuidv4();
 		return <React.Fragment>
 				<input id={labelId} type="checkbox" value={item} />
 				<label htmlFor={labelId}>{item}</label>
 			</React.Fragment>
-	}
-
+		}
 	);
 
 	return (
 		<div className={`foodCard foodCard--${title}`}>
 			<h3>{title} <i className={`fas fa-${icon}`}></i></h3>
 			<form>
-					{foodContents}
+				{foodContents}
 			</form>
 			<Addfood />
 		</div>
