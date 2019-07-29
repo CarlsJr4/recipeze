290a8f3158d76c0d8c4aca3c6e2db0677ed57861
Carl Dungca <carl.dungca@yahoo.com>
2019-07-29 13:11:49 -0700

    Added like button component to results cards

--- src/components/results/Resultcard.js
+++ src/components/results/Resultcard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Likebutton from './Likebutton';
 
 export default function Resultcard({title, img}) {
 	return (
 		<div className="resultCard">
 			<img src="https://picsum.photos/200" alt="recipe placeholder" />
+			<Likebutton />
 			<div className="resultCard__title">
 				<h3>{title}</h3>
 			</div>
