bd466d74fd75e3c4336a95f3a17c37b5ccb25362
Carl Dungca <carl.dungca@yahoo.com>
2019-07-29 12:18:22 -0700

    Changed Arrow button type to submit to allow for form submission

--- src/components/builder/Search.js
+++ src/components/builder/Search.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Search() {
 	return (
-		<button name="next"><i class="fas fa-arrow-right"></i></button>
+		<button type="submit"><i class="fas fa-arrow-right"></i></button>
 	)
 }
