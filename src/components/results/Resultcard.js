59ce4698b92421e7fbf6f76cd09a0fad4641f166
Carl Dungca <carl.dungca@yahoo.com>
2019-07-29 13:16:23 -0700

    Positioned heart button right absolute above results card titles

--- src/components/results/Resultcard.js
+++ src/components/results/Resultcard.js
@@ -5,8 +5,8 @@ export default function Resultcard({title, img}) {
 	return (
 		<div className="resultCard">
 			<img src="https://picsum.photos/200" alt="recipe placeholder" />
-			<Likebutton />
 			<div className="resultCard__title">
+				<Likebutton />
 				<h3>{title}</h3>
 			</div>
 		</div>
