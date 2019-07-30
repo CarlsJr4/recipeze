58bf7cdaba85982ea98760c8e2e9c05fe1cdc6e4
Carl Dungca <carl.dungca@yahoo.com>
2019-07-29 20:29:36 -0700

    Refactored app to load foods from useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Builder from './builder/Builder';
 import Results from './results/Results';
 import Recipe from './recipe/Recipe';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// Task: Load all foods from top-level app state
+
+// State should live at app level so that all router components can access it 
+// Maybe we should use the context API to pass data down
+
 function App() {
+	const allFoods = {
+			proteins: ['Beef', 'Chicken', 'Pork', 'Tofu'],
+			grains: ['White rice', 'Brown rice'],
+			veggies: ['Broccoli', 'Spinach', 'Brussels Sprouts'],
+			custom: ['Nuts']
+	}
+
+	const [foods, updateFood] = useState(allFoods);
+
   return (
 		<Router>
 			<div className="App">
-				{/* <Builder />
-				<Results />
-				<Recipe /> */}
-
-				<Route path="/" exact component={Builder} />
-				<Route path="/results" component={Results} />
-				<Route path="/recipe" component={Recipe} />
+				<Route 
+					path="/" 
+					render={() => <Builder foodList={foods} updateFood={updateFood}/>}
+					exact
+				/> 
+				<Route 
+					path="/results" 
+					component={Results} 
+				/>
+				<Route 
+					path="/recipe" 
+					component={Recipe} 
+				/>
 			</div>
 		</Router>
   );
--- src/components/builder/Builder.js
+++ src/components/builder/Builder.js
@@ -2,31 +2,33 @@ import React from 'react';
 import Foodcard from './Foodcard';
 import Search from './Search';
 
-const foods = [
-	{
-		proteins: ['Beef', 'Chicken', 'Pork', 'Tofu']
-	},
-	{
-		grains: ['White rice', 'Brown rice']
-	},
-	{
-		veggies: ['Broccoli', 'Spinach', 'Brussels Sprouts']
-	},
-	{
-		custom: ['Nuts']
-	}
-]
 
-export default function Builder() {	
+export default function Builder({foodList, updateFood}) {	
 	return (
 		<div>
 				<h1>MealBuilder</h1>
 				<h3>Pick foods from at least 3 categories</h3>
 				<div className="categories">
-					<Foodcard title="Protein" icon="drumstick-bite" contents={foods[0].proteins}/>
-					<Foodcard title="Grains" icon="bread-slice" contents={foods[1].grains}/>
-					<Foodcard title="Veggies" icon="carrot" contents={foods[2].veggies}/>
-					<Foodcard title="Custom" icon="utensils" contents={foods[3].custom}/>
+					<Foodcard 
+						title="Protein" 
+						icon="drumstick-bite" 
+						contents={foodList.proteins}
+					/>
+					<Foodcard 
+						title="Grains" 
+						icon="bread-slice" 
+						contents={foodList.grains}
+					/>
+					<Foodcard 
+						title="Veggies"
+						icon="carrot"
+						contents={foodList.veggies}
+					/>
+					<Foodcard 
+						title="Custom"
+						icon="utensils"
+						contents={foodList.custom}
+					/>
 					<Search />
 				</div>
 		</div>
