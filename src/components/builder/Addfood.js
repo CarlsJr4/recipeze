b294c1e3d4593019c360ad3eb23d07fce88e1421
Carl Dungca <carl.dungca@yahoo.com>
2019-07-29 20:49:59 -0700

    Merged an input form into the addFood component

--- src/components/builder/Addfood.js
+++ src/components/builder/Addfood.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Addfood() {
 	return (
-		<button name="add"><i class="fas fa-plus"></i></button>
+		<form>
+			<input type="text" />
+			<button name="add"><i class="fas fa-plus"></i></button>
+		</form>
 	)
 }
