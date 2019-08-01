6642854f58acc8153816c0ab3ac5d8ff79bc37ad
Carl Dungca <carl.dungca@yahoo.com>
2019-07-31 20:23:01 -0700

    Refactored reducers to use external functions for switch cases

--- src/context/reducers.js
+++ src/context/reducers.js
@@ -1,34 +1,40 @@
 // Should we refactor to use functions to preserve namespaces?
 // We should also refactor the second argument of reducer to just be 'action' to make the functions more specific
+// Reducer breaks when we move the function to global?
 
 // Next goal: Refactor this page. Make it cleaner, more readable, and more scalable.
 const uuidv4 = require('uuid/v4');
 
+function removeFood(state, category, id) {	
+	const modifiedState = Object.assign({}, state);  // Copy the state object
+	const stateCategory = [...modifiedState[category]];  // Copy the array of the category of food to modify 
+	const filteredCategory = stateCategory.filter(ingredient => // Filter the array
+		ingredient.id !== id
+	)
+	// Assign new array to modified state
+	modifiedState[category] = filteredCategory; 
+	// Update state
+	return modifiedState; 
+}
+
+function addFood(state, category, inputValue) {
+	const modifiedState = Object.assign({}, state); // Copy of object
+	const stateCategory = [...modifiedState[category]]; // Array of category
+	stateCategory.push({ // Push to array
+		name: inputValue,
+		id: uuidv4()
+	})
+	modifiedState[category] = stateCategory;
+	return modifiedState;
+}
+
 // {type, id, category, inputValue}
 const reducer = (state, action) => {
 	switch (action.type) {
-
 		case 'remove_food':
-			const modifiedState = Object.assign({}, state);  // Copy the state object
-			const stateCategory = [...modifiedState[action.category]];  // Copy the array of the category of food to modify 
-			const filteredCategory = stateCategory.filter(ingredient => // Filter the array
-				ingredient.id !== action.id
-			)
-			// Assign new array to modified state
-			modifiedState[action.category] = filteredCategory; 
-			// Update state
-			return modifiedState; 
-
+			return removeFood(state, action.category, action.id);
 		case 'add_food':
-				const inputState = Object.assign({}, state); // Copy of object
-				const inputStateCategory = [...inputState[action.category]]; // Array of category
-				inputStateCategory.push({ // Push to array
-					name: action.inputValue,
-					id: uuidv4()
-				})
-				inputState[action.category] = inputStateCategory;
-				return inputState;
-
+			return addFood(state, action.category, action.inputValue)
 		default: throw new Error()
 	}
 }
