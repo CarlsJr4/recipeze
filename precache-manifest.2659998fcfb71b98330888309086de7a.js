32ab14d39835d7e340660e89c58b4091aa035afd
Carl Dungca <carl.dungca@yahoo.com>
2019-08-08 10:24:18 -0700

    Updates

---
+++ precache-manifest.2659998fcfb71b98330888309086de7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e08d8dfb8b26b8668871da3359cde1f",
+    "url": "/mealbuilder/index.html"
+  },
+  {
+    "revision": "edbf352266edb464766c",
+    "url": "/mealbuilder/static/css/2.b3a71e39.chunk.css"
+  },
+  {
+    "revision": "3c923d845d2f6e141295",
+    "url": "/mealbuilder/static/css/main.f7fbe88b.chunk.css"
+  },
+  {
+    "revision": "edbf352266edb464766c",
+    "url": "/mealbuilder/static/js/2.a391ab56.chunk.js"
+  },
+  {
+    "revision": "3c923d845d2f6e141295",
+    "url": "/mealbuilder/static/js/main.b0d5d155.chunk.js"
+  },
+  {
+    "revision": "fb72ed994851a9a3b422",
+    "url": "/mealbuilder/static/js/runtime~main.5f36f171.js"
+  }
+]);
