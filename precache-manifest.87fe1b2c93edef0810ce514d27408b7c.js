f7a0cc11f98af2654cb83194b4a6775a1ed615b0
Carl Dungca <carl.dungca@yahoo.com>
2022-10-16 22:04:54 -0700

    Updates

---
+++ precache-manifest.87fe1b2c93edef0810ce514d27408b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099bef4808dbf7510b9ac0799a6bc9aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aecba285ab6ded366625",
+    "url": "/static/css/2.055adff4.chunk.css"
+  },
+  {
+    "revision": "5e35e4621e003adf7b26",
+    "url": "/static/css/main.8a1f128c.chunk.css"
+  },
+  {
+    "revision": "aecba285ab6ded366625",
+    "url": "/static/js/2.195d00ac.chunk.js"
+  },
+  {
+    "revision": "5e35e4621e003adf7b26",
+    "url": "/static/js/main.a1b306be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
