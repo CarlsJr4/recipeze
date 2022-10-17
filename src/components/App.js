a278459d9a8246879ba35b657c14ef2eab8972f3
Carl Dungca <carl.dungca@yahoo.com>
2022-10-16 22:20:55 -0700

    Adds baseline to router

--- src/components/App.js
+++ src/components/App.js
@@ -5,28 +5,17 @@ import Results from './results/Results';
 import Recipe from './recipe/Recipe';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
-
 function App() {
   return (
-		<GlobalState>
-			<Router>
-				<div className="App">
-					<Route 
-						path="/" 
-						component={Builder}
-						exact
-					/> 
-					<Route 
-						path="/results" 
-						component={Results} 
-					/>
-					<Route 
-						path="/recipe" 
-						component={Recipe} 
-					/>
-				</div>
-			</Router>
-		</GlobalState>
+    <GlobalState>
+      <Router baseline="/">
+        <div className="App">
+          <Route path="/" component={Builder} exact />
+          <Route path="/results" component={Results} />
+          <Route path="/recipe" component={Recipe} />
+        </div>
+      </Router>
+    </GlobalState>
   );
 }
 
