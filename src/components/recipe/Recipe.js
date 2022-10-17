4ddc54bbf6804598196719cdc6da8b069d6be229
Carl Dungca <carl.dungca@yahoo.com>
2022-10-16 21:52:17 -0700

    Fixes blank ingredients

--- src/components/recipe/Recipe.js
+++ src/components/recipe/Recipe.js
@@ -4,78 +4,83 @@ import Loader from '../Loader';
 import Homebutton from '../recipe/Homebutton';
 import Original from '../recipe/Original';
 
-
 export default function Recipe() {
-	const globalState = useContext(FoodContext);
-	const {
-		servings,
-		extendedIngredients,
-		title,
-		readyInMinutes,
-		image,
-		instructions,
-		analyzedInstructions,
-		sourceUrl 
-	} = globalState.recipeInfo;
+  const globalState = useContext(FoodContext);
+  const {
+    servings,
+    extendedIngredients,
+    title,
+    readyInMinutes,
+    image,
+    instructions,
+    analyzedInstructions,
+    sourceUrl,
+  } = globalState.recipeInfo;
 
-	// Check if ideal instructions format exists, and render them if they do
-	let displayedInstructions
-	if (analyzedInstructions && analyzedInstructions.length > 0) {
-		const directions = analyzedInstructions[0].steps;
-		displayedInstructions = directions.map(step => <p>{step.number}. {step.step}</p>);
-	} else if (instructions) {
-		displayedInstructions = <p>{instructions}</p>
-	} else {
-		displayedInstructions = <p>Instructions only available on original recipe (see below).</p>
-	}
+  // Check if ideal instructions format exists, and render them if they do
+  let displayedInstructions;
+  if (analyzedInstructions && analyzedInstructions.length > 0) {
+    const directions = analyzedInstructions[0].steps;
+    displayedInstructions = directions.map(step => (
+      <p>
+        {step.number}. {step.step}
+      </p>
+    ));
+  } else if (instructions) {
+    displayedInstructions = <p>{instructions}</p>;
+  } else {
+    displayedInstructions = (
+      <p>Instructions only available on original recipe (see below).</p>
+    );
+  }
 
-	// When navigating away from this page, clear the recipe from globalState
-	useEffect(() => 
-		function cleanup() {
-			return globalState.setRecipeInfo({})
-		}, []
-	)
+  // When navigating away from this page, clear the recipe from globalState
+  useEffect(
+    () =>
+      function cleanup() {
+        return globalState.setRecipeInfo({});
+      },
+    []
+  );
 
-	let ingredients 
-	if (extendedIngredients) {
-		ingredients = extendedIngredients.map(ingredient => 
-			<li>{ingredient.originalString}</li>
-		)
-	} else ingredients = <li>Loading ingredients...</li>
+  let ingredients;
+  if (extendedIngredients) {
+    ingredients = extendedIngredients.map(ingredient => (
+      <li>{ingredient.original}</li>
+    ));
+  } else ingredients = <li>Loading ingredients...</li>;
 
-	if (Object.getOwnPropertyNames(globalState.recipeInfo).length === 0) {
-		return <Loader />
-	}
+  if (Object.getOwnPropertyNames(globalState.recipeInfo).length === 0) {
+    return <Loader />;
+  }
 
-	return (
-		<div className="recipe">
-			<h1>{title}</h1>
+  return (
+    <div className="recipe">
+      <h1>{title}</h1>
 
-			<div className="recipe__summary">
-				<img src={image} alt={title} />
-				<div className="recipe__ingredients">
-					<h3>Preparation time: </h3>
-						 <p>{readyInMinutes} minutes</p>
-					<h3>Number of servings: </h3>
-					<p>{servings}</p>
-					<h3>Ingredients:</h3>
-					<ul>
-						{ingredients}
-					</ul>
-				</div>
-			</div>
+      <div className="recipe__summary">
+        <img src={image} alt={title} />
+        <div className="recipe__ingredients">
+          <h3>Preparation time: </h3>
+          <p>{readyInMinutes} minutes</p>
+          <h3>Number of servings: </h3>
+          <p>{servings}</p>
+          <h3>Ingredients:</h3>
+          <ul>{ingredients}</ul>
+        </div>
+      </div>
 
-			<div className="recipe__instructions">
-				<h3>Instructions:</h3>
-				{displayedInstructions}
-			</div>
+      <div className="recipe__instructions">
+        <h3>Instructions:</h3>
+        {displayedInstructions}
+      </div>
 
-			<br></br>
+      <br></br>
 
-			<div className="recipe__buttons">
-				<Homebutton />
-				<Original link={sourceUrl}/>
-			</div>
-		</div>
-	)
+      <div className="recipe__buttons">
+        <Homebutton />
+        <Original link={sourceUrl} />
+      </div>
+    </div>
+  );
 }
